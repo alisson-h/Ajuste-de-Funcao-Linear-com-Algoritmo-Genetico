68a62324ab9120df765972d1a58cde383e04f0ba
alisson-h <ali123906123@gmail.com>
2026-04-03 09:30:46 -0300

    Adcionado Cabecalho

--- src/main.c
+++ src/main.c
@@ -1,3 +1,18 @@
+/** ===========================================================================================
+ *
+ * Trabalho Aquecimento de Algoritmos e Estruturas de Dados
+ * 
+ *  Objetivo: Implementar um algoritmo genético para resolver um problema de ajuste de curva,
+ *  onde o objetivo é encontrar os coeficientes de uma reta (y = ax + b) que melhor se ajusta
+ *  a um conjunto de pontos (x, y) usando o Erro Absoluto Médio (MAE) como função de fitness.
+ * 
+ * Aluno: Alisson Henrique Gomes
+ * Data: 05/04/2026
+ * 
+ * Prof: Michael Pires Silva
+ * 
+ * ========================================================================================== */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
